ba4db35db71d97aa8e20651fde1ef16bf69a779d
zihadul <zihadulislam41@gmail.com>
2015-05-13 16:09:37 +0600

    initial file

---
+++ OneToHundredEvenNumberApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneToHundredEvenNumberApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int num = 1; num <= 100; num++)
+            {
+                if (num%2==0)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
